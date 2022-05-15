f63903bb40284f026d6f1de297740a04e81d6068
Becks Simpson <becks.simpson@outlook.com>
2022-05-15 02:16:09 +0100

    Functional adding people/characters popups. Need more validation, need to display characters/people.

--- qwatch/gui/people.py
+++ qwatch/gui/people.py
@@ -1,6 +1,9 @@
+from functools import partial
 import logging
 from typing import Callable, Dict, List
+import uuid
 
+import pandas as pd
 import tkinter as tk
 from tkinter import ttk
 
@@ -17,6 +20,7 @@ from qwatch.gui.menus import MenuMultiSelector, MenuSingleSelector
 
 
 logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 
 class PersonPage(ttk.Frame):
@@ -30,9 +34,25 @@ class PersonPage(ttk.Frame):
         """
         ttk.Frame.__init__(self, parent)
 
+        if is_character:
+            self.people = [] if people.get(
+                "characters", None) is None else people["characters"]
+            self.actors = [] if people.get(
+                "people", None) is None else people["people"]
+
+            # TODO Get actors only
+            # else [
+            #       p for p in people["people"]
+            #       if "ACTOR" in p["ROLES"]
+            #     ]
+        else:
+            self.people = [] if people.get(
+                "people", None) is None else people["people"]
+
         self.update_people = update_people
-        self.people = [] if people is None else people
         self.is_character = is_character
+
+        self.get_people_properties()
 
         personHeader = ttk.Frame(self)
         personHeader.pack(side="top", padx=5, pady=5, fill=tk.X)
@@ -41,11 +61,13 @@ class PersonPage(ttk.Frame):
             lbl = "Insert Characters in movie"
         else:
             lbl = "Insert People working on the movie"
+
         ttk.Label(personHeader, text=lbl).pack(
             side="left", padx=(0, 10))
-        ttk.Button(personHeader, text="+", command=lambda: CreatePerson(self.save_person, is_character=self.is_character)).pack(
+        ttk.Button(personHeader, text="+", command=lambda: CreatePerson(self.save_person, is_character=self.is_character, options=self.OPTIONS)).pack(
             side="left", pady=5, padx=5
         )
+
         self.personContainer = ttk.Frame(self)
 
         self.personContainer.pack(
@@ -55,38 +77,127 @@ class PersonPage(ttk.Frame):
         for person in self.people:
             self.add_person_ui(person)
 
-    def save_person(self, person):
-        logger.debug(f"Saving person in {__class__}: {person}")
+    def refresh_ui(self):
+        for widget in self.personContainer.winfo_children():
+            widget.destroy()
+
+        for person in self.people:
+            self.add_person_ui(person)
+
+    def delete_person(self, person):
+        """ Delete person from list."""
+        logger.debug(f"Deleting person in {__class__}: {person}")
+
         self.people = [
-            *self.people, person
+            p for p in self.people if p["ID"] != person["ID"]
         ]
+
+        self.refresh_ui()
 
         # Update People lists for other people management pages
         prop = "characters" if self.is_character else "people"
         self.update_people({prop: self.people})
 
-        # TODO: Update ui
-        self.add_person_ui(person)
+    def save_person(self, person):
+        logger.debug(f"Saving person in {__class__}: {person}")
+
+        # Pre-existing person, edited
+        if person["ID"] in [p["ID"] for p in self.people]:
+            self.people = [
+                p if p["ID"] != person["ID"] else person for p in self.people
+            ]
+            self.refresh_ui()
+        # New person
+        else:
+            self.people = [
+                *self.people, person
+            ]
+            # TODO: Update ui
+            self.add_person_ui(person)
+
+        # Update People lists for other people management pages
+        prop = "characters" if self.is_character else "people"
+        self.update_people({prop: self.people})
 
     def add_person_ui(self, person):
         """Add person to ui"""
         logger.debug(f"Adding person to UI in {__class__}: {person}")
+        print(person)
         personFrame = ttk.Frame(self.personContainer)
-        tk.Label(personFrame, text=f"{person['FIRST_NAME']} {person['LAST_NAME']}").grid(
-            row=0, column=0)
-        personFrame.pack(side=tk.TOP, expand=1, fill=tk.X, padx=5, pady=(0, 5))
+        # Name
+        ttk.Label(personFrame, text=f"{person['FIRST_NAME']} {person['LAST_NAME']}").grid(
+            row=0, column=0, padx=5, pady=(0, 2))
+        if not self.is_character:
+            #
+            ttk.Label(personFrame, text=self.get_options(
+                person, "ROLES")).grid(row=1, column=0, padx=5, pady=(0, 5))
+
+        ################################################
+        # Command Panel
+        ################################################
+        ttk.Button(
+            personFrame, text="Remove", command=partial(self.delete_person, person["ID"])
+        ).grid(column=3, padx=(20, 0), sticky=tk.E, row=0)
+
+        ttk.Button(
+            personFrame, text="Edit", command=lambda: CreatePerson(self.save_person, is_character=self.is_character, person=person, options=self.OPTIONS)
+        ).grid(column=3, padx=(20, 0), sticky=tk.E, row=1)
+
+    def update_contents(self, people):
+        if self.is_character and "people" in people:
+            self.actors = [] if people.get(
+                "people", None) is None else people["people"]
+
+    def get_options(self, person: Dict, option: str):
+
+        if isinstance(person[option], list):
+            return ", ".join([
+                f"{row.LABEL} - {row.SUB_LABEL or 'NULL'}" if "SUB_LABEL" in row.index else row.LABEL
+                for _, row in self.OPTIONS[option].iterrows()
+                if row.ID in person[option]
+            ])
+        else:
+            row = self.OPTIONS[option][self.OPTIONS[option].ID ==
+                                       person[option]].iloc[0, :]
+            return f"{row.LABEL} - {row.SUB_LABEL or 'NULL'}" if "SUB_LABEL" in row.index else row.LABEL
+
+    def get_people_properties(self):
+        """ Retrieve person mappings from db."""
+        engine = _create_engine()
+        with engine.connect() as conn:
+            genders = _get_movie_properties(conn, "GENDER")
+            ethnicities = _get_movie_properties(conn, "ETHNICITIE")
+            sexualities = _get_movie_properties(conn, "SEXUALITIE")
+            roles = _get_movie_properties(conn, "ROLE")
+            disabilities = _get_movie_properties(conn, "DISABILITIE")
+            careers = _get_movie_properties(conn, "CAREER")
+            transgenders = _get_movie_properties(conn, "TRANSGENDER")
+            self.OPTIONS = {
+                "GENDERS": genders,
+                "ETHNICITIES": ethnicities,
+                "SEXUALITIES": sexualities,
+                "ROLES": roles,
+                "DISABILITIES": disabilities,
+                "CAREER": careers,
+                "TRANSGENDER": transgenders,
+            }
 
 
 class CreatePerson(tk.Toplevel):
-    def __init__(self, save_action, is_character: bool, person: Dict = None):
+    def __init__(self, save_action, is_character: bool, person: Dict = None, actors: List[Dict] = None, options: Dict = None):
         tk.Toplevel.__init__(self, pady=5, padx=5)
         self.wm_title(
             f"Create {'Person' if not is_character else 'Character'}")
 
         self.person = person if person is not None else {}
+        self.actors = actors if actors is not None else []
         self.is_character = is_character
         self.save_action = save_action
-        self.get_people_properties()
+
+        if options is None:
+            self.get_people_properties()
+        else:
+            self.OPTIONS = options
 
         self.create_ui()
 
@@ -101,19 +212,23 @@ class CreatePerson(tk.Toplevel):
             return
 
         self.person = {
+            **self.person,  # To overwrite, keep unaltered qualities
             "FIRST_NAME": self.first_name.get(),
             "LAST_NAME": self.last_name.get(),
             "BIO": self.bio.get("1.0", tk.END),
             "ETHNICITIES": self.ethnicity_menu.get_selected_options(),
-            "SEXUALITIES": self.sexuality_menu.get_selected_options(),
+            "SEXUALITIES": self.sexuality_menu.get_selected_option(),
             "DISABILITIES": self.disability_menu.get_selected_options(),
-            "GENDERS": self.gender_menu.get_selected_options(),
+            "GENDERS": self.gender_menu.get_selected_option(),
             "TRANSGENDER": self.transgender_menu.get_selected_option(),
         }
         if self.is_character:
             self.person = {
                 **self.person,
-                "CAREER": self.career_menu.get_selected_option()
+                "CAREER": self.career_menu.get_selected_option(),
+                "HAIR_COLOR": self.hair_color.get(),
+                "MAIN": self.main.get(),
+                "ACTOR": self.actor_menu.get_selected_option(),
             }
         else:
             self.person = {
@@ -121,6 +236,10 @@ class CreatePerson(tk.Toplevel):
                 "DOB": self.dob.get(),
                 "ROLES": self.role_menu.get_selected_options(),
             }
+
+        # Temporary unique identifier
+        if "ID" not in self.person:
+            self.person["ID"] = str(uuid.uuid4())
 
         self.save_action(self.person)
         self.destroy()
@@ -149,35 +268,44 @@ class CreatePerson(tk.Toplevel):
 
         # DOB
         if not self.is_character:
-            ttk.Label(self, text="DOB").grid(column=0, row=2, sticky=tk.W)
+            ttk.Label(self, text="Date of Birth").grid(
+                column=0, row=2, sticky=tk.W)
             self.dob = ttk.Entry(self, width=30)
             self.dob.grid(column=1, row=2, sticky=tk.W,
                           padx=(0, 10), pady=(0, 5))
             if "DOB" in self.person:
                 self.dob.insert(0, self.person["DOB"])
+        else:
+            ttk.Label(self, text="Hair Color").grid(
+                column=0, row=2, sticky=tk.W)
+            self.hair_color = ttk.Entry(self, width=30)
+            self.hair_color.grid(column=1, row=2, sticky=tk.W,
+                                 padx=(0, 10), pady=(0, 5))
+            if "HAIR_COLOR" in self.person:
+                self.hair_color.insert(0, self.person["HAIR_COLOR"])
 
         button_frame = ttk.Frame(self)
 
-        self.sexuality_menu = MenuMultiSelector(
-            button_frame, "Sexuality", self.OPTIONS["sexualities"],
-            default=self.person.get("SEXUALITIES", [])
+        self.sexuality_menu = MenuSingleSelector(
+            button_frame, "Sexuality", self.OPTIONS["SEXUALITIES"],
+            default=self.person.get("SEXUALITIES", None)
         )
         self.sexuality_menu.pack(
             side="left", fill=tk.X, expand=True, padx=(0, 5))
 
-        self.gender_menu = MenuMultiSelector(
-            button_frame, "Gender", self.OPTIONS["genders"],
-            default=self.person.get("GENDERS", []))
+        self.gender_menu = MenuSingleSelector(
+            button_frame, "Gender", self.OPTIONS["GENDERS"],
+            default=self.person.get("GENDERS", None))
         self.gender_menu.pack(side="left", fill=tk.X, expand=True, padx=(0, 5))
 
         self.ethnicity_menu = MenuMultiSelector(
-            button_frame, "Ethnicity", self.OPTIONS["ethnicities"],
+            button_frame, "Ethnicity", self.OPTIONS["ETHNICITIES"],
             default=self.person.get("ETHNICITIES", []))
         self.ethnicity_menu.pack(
             side="left", fill=tk.X, expand=True, padx=(0, 5))
 
         self.disability_menu = MenuMultiSelector(
-            button_frame, "Disability", self.OPTIONS["disabilities"],
+            button_frame, "Disability", self.OPTIONS["DISABILITIES"],
             default=self.person.get("DISABILITIES", [])
         )
         self.disability_menu.pack(side="left", fill=tk.X, expand=True)
@@ -189,30 +317,42 @@ class CreatePerson(tk.Toplevel):
         # Transgender, Role
         #########################################################
         level2 = ttk.Frame(self)
-        # self.transgender = tk.BooleanVar(
-        #     value=self.person.get("transgender", False))
-        # transgender_btn = ttk.Checkbutton(
-        #     level2, text="Transgender", variable=self.transgender,
-        #     style='Switch.TCheckbutton'
-        # )
-        # transgender_btn.pack(
-        #     side="left", fill=tk.X, expand=True, padx=(0, 5))
+        if len(self.actors):
+            actors = pd.DataFrame([])
+            actors.loc[:, "LABEL"] = [
+                f'{a["FIRST_NAME"]} {a["LAST_NAME"]}' for a in self.actors]
+            actors.loc[:, "ID"] = [a["ID"] for a in self.actors]
+            self.actor_menu = MenuSingleSelector(
+                level2, "Actor", actors, default=self.person.get("ACTOR", None)
+            )
+            self.actor_menu.pack(
+                side="left", fill=tk.X, expand=True, padx=(0, 5)
+            )
+
         self.transgender_menu = MenuSingleSelector(
-            level2, "Transgender", self.OPTIONS["transgenders"], default=self.person.get("TRANSGENDER", None)
+            level2, "Transgender", self.OPTIONS["TRANSGENDER"], default=self.person.get("TRANSGENDER", None)
         )
         self.transgender_menu.pack(
             side="left", fill=tk.X, expand=True, padx=(0, 5))
 
         if not self.is_character:
             self.role_menu = MenuMultiSelector(
-                level2, "Roles", self.OPTIONS["roles"],
+                level2, "Roles", self.OPTIONS["ROLES"],
                 default=self.person.get("ROLES", []))
             self.role_menu.pack(side="left", fill=tk.X, expand=True)
         else:
             self.career_menu = MenuSingleSelector(
-                level2, "Careers", self.OPTIONS["careers"], default=self.person.get("CAREER", None)
+                level2, "Careers", self.OPTIONS["CAREER"], default=self.person.get("CAREER", None)
             )
             self.career_menu.pack(side="left", fill=tk.X, expand=True)
+
+            self.main = tk.IntVar(value=self.person.get("MAIN", 0))
+            main_btn = ttk.Checkbutton(
+                level2, text="Main Character", variable=self.main,
+                style='Switch.TCheckbutton'
+            )
+            main_btn.pack(
+                side="left", fill=tk.X, expand=True, padx=(0, 5))
 
         level2.grid(column=0, row=4, pady=(5, 0),
                     rowspan=1, columnspan=4, sticky="ew")
@@ -245,13 +385,13 @@ class CreatePerson(tk.Toplevel):
             careers = _get_movie_properties(conn, "CAREER")
             transgenders = _get_movie_properties(conn, "TRANSGENDER")
             self.OPTIONS = {
-                "genders": genders,
-                "ethnicities": ethnicities,
-                "sexualities": sexualities,
-                "roles": roles,
-                "disabilities": disabilities,
-                "careers": careers,
-                "transgenders": transgenders,
+                "GENDERS": genders,
+                "ETHNICITIES": ethnicities,
+                "SEXUALITIES": sexualities,
+                "ROLES": roles,
+                "DISABILITIES": disabilities,
+                "CAREER": careers,
+                "TRANSGENDER": transgenders,
             }
 
 
@@ -268,9 +408,9 @@ class PeopleManagementPanel(ttk.Frame):
 
         self.notebook = ttk.Notebook(self, height=300)
         self.personPage = PersonPage(
-            self.notebook, self.update_people_overview, is_character=False, people=self.people_overview["people"])
+            self.notebook, self.update_people_overview, is_character=False, people=self.people_overview)
         self.characterPage = PersonPage(
-            self.notebook, self.update_people_overview, is_character=True, people=self.people_overview["characters"])
+            self.notebook, self.update_people_overview, is_character=True, people=self.people_overview)
 
         # TODO: Own classes
         self.relationshipPage = ttk.Frame(self.notebook)
@@ -292,5 +432,10 @@ class PeopleManagementPanel(ttk.Frame):
         Called to subpages, to update their contents with new people.
         Depending on which properties have been updated.
         """
+        if "people" in people:
+            self.characterPage.update_contents(people)
+            # TODO update relationships options
 
-        pass
+        if "characters" in people:
+            # TODO Update relationships/actions options
+            logger.debug("Update relationships/actions")
